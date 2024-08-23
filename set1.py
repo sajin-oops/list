efc91210d5fc1a2f8f4521e2c5f8de1aa17ea7d7
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-08-23 20:03:58 +0530

    Add files via upload

---
+++ set1.py
@@ -0,0 +1,9 @@
+mylist = ["sajin","lijin","aaron"]
+print(mylist)
+#O/P - ['sajin', 'lijin', 'aaron']
+mylist.append("nijas")
+print(mylist)
+#O/P - ['sajin', 'lijin', 'aaron', 'nijas']
+x = mylist.count("sajin")
+print(x)
+#O/P - 1
